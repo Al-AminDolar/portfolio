dafee458f97739796a3879f22ebecbaee2a76329
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-11 23:51:49 +0600

    ok

--- src/component/main/Caurosel.jsx
+++ src/component/main/Caurosel.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import { BiRightArrowAlt } from "react-icons/bi";
 import Slider from "react-slick";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import data from "../dummy/dummyProducts.json";
 
 const Caurosel = () => {
+  const CustomPrevArrow = ({ onClick }) => (
+    <button className="custom-prev-arrow bg-white">Prev</button>
+  );
+
+  const CustomNextArrow = ({ onClick }) => (
+    <button className="custom-next-arrow bg-white" onClick={onClick}>
+      Next
+    </button>
+  );
+
   const { popularProducts } = data;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
   return (
     <div className=" flex justify-center pb-44">
       <div className="bg-[#131c31] h-[240px] w-[900px] border border-gray-800  rounded-md shadow-sm p-5 grid grid-cols-5">
@@ -20,7 +40,7 @@ const Caurosel = () => {
           </p>
 
           <div className="text-gray-200 flex gap-3 ">
-            <span className="p-1 border border-gray-600 rounded-full bg-[#1d222e] hover:bg-[#0dcaf0] cursor-pointer">
+            <span className="p-1 border border-gray-600 rounded-full bg-[#1d222e] hover:bg-[#0dcaf0] cursor-pointer ">
               <BiLeftArrowAlt size={20} />
             </span>
             <span className="p-1 border border-gray-600 rounded-full bg-[#1d222e] hover:bg-[#0dcaf0] cursor-pointer">
@@ -28,23 +48,25 @@ const Caurosel = () => {
             </span>
           </div>
         </div>
-        <div className="col-span-4  p-5 ">
-          <div className="flex  justify-center items-center">
-            {popularProducts?.slice(0, 4).map((data, i) => {
+        <div className="col-span-4  p-10 bg-white rounded ">
+          <Slider {...settings}>
+            {popularProducts?.slice(0, 3).map((data, i) => {
               return (
-                <div className="h-10 w-30 " key={i}>
-                  <div className="flex flex-col item-center text-center cursor-pointer  mx-[5%]">
+                <div className=" " key={i}>
+                  <div className="flex  flex-col justify-center item-center text-center cursor-pointer  mx-[5%] ">
                     <img
                       src={data?.img}
                       alt=""
-                      className="hover:outline outline-2 outline-[rgb(13,173,230)] rounded-lg h-[150px] w-[220px]"
+                      className="hover:outline outline-2 outline-[rgb(13,173,230)] rounded-lg h-[80%] w-[80%]"
                     />
-                    <h3 className="text-sm text-white">{data?.title}</h3>
+                    <h3 className="text-sm h-[80%] w-[80%]  text-black">
+                      {data?.title}
+                    </h3>
                   </div>
                 </div>
               );
             })}
-          </div>
+          </Slider>
         </div>
       </div>
     </div>
