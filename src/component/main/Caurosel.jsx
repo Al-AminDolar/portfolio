3500feee7261f0079a28c2841b8d3c42987b2f45
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-11 22:42:08 +0600

    in processing

--- src/component/main/Caurosel.jsx
+++ src/component/main/Caurosel.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import { BiRightArrowAlt } from "react-icons/bi";
 import Slider from "react-slick";
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-};
+
+import data from "../dummy/dummyProducts.json";
+
 const Caurosel = () => {
+  const { popularProducts } = data;
   return (
     <div className=" flex justify-center pb-44">
       <div className="bg-[#131c31] h-[240px] w-[900px] border border-gray-800  rounded-md shadow-sm p-5 grid grid-cols-5">
@@ -31,20 +28,23 @@ const Caurosel = () => {
             </span>
           </div>
         </div>
-        <div className="col-span-4 ">
-          <Slider {...settings} className="">
-            <div className="h-300 w-300  bg-red-600 p-10 text-black">
-              <h3>Dolar</h3>
-            </div>
-
-            <div className="h-300 w-300 bg-slate-500 p-10 text-black">
-              <h3>ddddddd</h3>
-            </div>
-
-            <div className="h-300 w-300 bg-green-500 p-10 text-black">
-              <h3>ssss3</h3>
-            </div>
-          </Slider>
+        <div className="col-span-4  p-5 ">
+          <div className="flex  justify-center items-center">
+            {popularProducts?.slice(0, 4).map((data, i) => {
+              return (
+                <div className="h-10 w-30 " key={i}>
+                  <div className="flex flex-col item-center text-center cursor-pointer  mx-[5%]">
+                    <img
+                      src={data?.img}
+                      alt=""
+                      className="hover:outline outline-2 outline-[rgb(13,173,230)] rounded-lg h-[150px] w-[220px]"
+                    />
+                    <h3 className="text-sm text-white">{data?.title}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
