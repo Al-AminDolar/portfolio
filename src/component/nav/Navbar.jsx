a74d496b51a60c7aee00c178d44dfc81c761f205
alamindolar <amin15-9898@diu.edu.bd>
2023-04-27 16:48:41 +0600

    navbar fixed top

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="">
-      <div className="bg-blueGray-900 h-20 flex items-center justify-between px-32">
+    <div className="fixed  top-0 left-0 right-0 z-10">
+      <div className="bg-blueGray-900 h-20 flex items-center justify-between px-32  ">
         <div>
           <p className="text-white text-3xl font-sans">Soft-Tech-Programmer</p>
         </div>
